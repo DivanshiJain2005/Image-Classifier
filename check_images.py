2065c8f5272b190e5ffba51ed178f983dc99a2d6
Divanshi Jain <121867251+DivanshiJain2005@users.noreply.github.com>
2023-07-12 23:06:20 +0530

    Update check_images.py

--- check_images.py
+++ check_images.py
@@ -3,8 +3,8 @@
 # */AIPND-revision/intropyproject-classify-pet-images/check_images.py
 #
 # TODO 0: Add your information below for Programmer & Date Created.                                                                             
-# PROGRAMMER: Rakshit Agrawal
-# DATE CREATED:                                  16/12/2022
+# PROGRAMMER: Divanshi Jain
+# DATE CREATED: 06/07/2023
 # REVISED DATE: 
 # PURPOSE: Classifies pet images using a pretrained CNN model, compares these
 #          classifications to the true identity of the pets in the images, and
